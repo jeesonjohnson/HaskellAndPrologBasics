967c77cc308dc8fdb3dc4060a54c7903ed944a53
Jeeson Johnson <jeesonjohnson@github.com>
2020-01-02 21:43:26 +0000

    Completed Q3.1.a

--- question3.pl
+++ question3.pl
@@ -1,3 +1,41 @@
+%-----------------------------------------------------------------------------------------
+% Question 3.1
+%-----------------------------------------------------------------------------------------
+%##########################################################################################
+% Question 3.1.a
+%  Defining translation facts.
+%##########################################################################################
+english_french(mad,fou).%My name is Jeeson and i love computers. I also love a good chocolate cheescake.
+english_french(my,ma).
+english_french(name,nom).
+english_french(is,est).
+english_french(and,et).
+english_french(i,je).
+english_french(love,lamour).
+english_french(computers,ordinateurs).
+english_french(also,aussi).
+english_french(good,bein).
+english_french(chocolate,chocolat).
+%##########################################################################################
+% Question 3.1.b
+%  Method for translate between french and english
+%##########################################################################################
+
+translate_eng([],[]).
+translate_eng([WordHead|WordTail],Answer).
+
+
+
+
+
+
+
+%-----------------------------------------------------------------------------------------
+% Question 3.2
+%-----------------------------------------------------------------------------------------
+%##########################################################################################
+% Question 3.2.a
+%##########################################################################################
 % Distance represents the raw distance, in a bi nature fastion of the given nodes
 % The distances of all nodes respective to c1.
 distance(c1,c2,4).
@@ -22,37 +60,70 @@ distance(c5,c3,2).
 distance(c4,c5,8).
 distance(c5,c4,8).
 
-% Question 3.2.c Solution!
-path(Start,End,[End|GenTail],[End|GenTail]):-member(Start,[End|GenTail]). %Check that the last element in list, is the final answer we were looking for.
-path(Start,End,[GenHead|GenTail],Answer):-distance(Start,SecondStep,_),
+
+
+%##########################################################################################
+% Question 3.2.b
+%##########################################################################################
+% The bidirectional nature of the graph was fixed, by implmenting a reverse perdicate, that represents the opposite direction.
+% For example to make c1 to c2 bidirectional, we first define distance(c1,c2,4) and then distance(c2,c1,4).
+% But to ensure the below predicates do not become stuck in a loop, we need to ensrue that a previously
+% visted node is not met again by the alargorithm.
+
+
+
+%##########################################################################################
+% Question 3.2.c
+%##########################################################################################
+path(Start,End,[End|GenTail],[End|GenTail]):-
+                          %Check that the last element in list, is the final answer we were looking for.
+                          member(Start,[End|GenTail]).
+path(Start,End,[GenHead|GenTail],Answer):-
+                          distance(Start,SecondStep,_),
+                          \+member(SecondStep,[GenHead|GenTail]), % Ensures no loops.
+                          path(SecondStep,End,[SecondStep,GenHead|GenTail],Answer).
+
+% ######### Main Function ###############
+% Returns to the user the all paths, in an appropriate order.
+find_paths(Start,End,Answer):-
+                   path(Start,End,[Start],GenAnswer),
+                   reverse(GenAnswer,ReversedAnswer),
+                   Answer = ReversedAnswer.
+
+
+
+%##########################################################################################
+% Question 3.2.d
+%##########################################################################################
+head([H|_], H). %Finds the top element of a list
+head((Length,Route),Length,Route). % Sperates the first element of a tuple into 2 elements.
+
+% Generates all paths, along with thier associated lengths, using recursion.
+path_gen(Start,End,[End|GenTail],[End|GenTail],Length,Length):-
+                                    member(Start,[End|GenTail]).
+path_gen(Start,End,[GenHead|GenTail],Answer,OrignalLength,Length):-
+                                    distance(Start,SecondStep,GeneratedDistance),
                                     \+member(SecondStep,[GenHead|GenTail]),
-                                    path(SecondStep,End,[SecondStep,GenHead|GenTail],Answer).
-
-
-find_paths(Start,End,Answer):- path(Start,End,[Start],GenAnswer),
-                               reverse(GenAnswer,ReversedAnswer),
-                               Answer = ReversedAnswer.
-
-% Question 3.2.b Solution!
-head([H|_], H). %Finds the top element of a list.
-head((Length,Route),Length,Route).
-
-path_gen(Start,End,[End|GenTail],[End|GenTail],Length,Length):-member(Start,[End|GenTail]).
-path_gen(Start,End,[GenHead|GenTail],Answer,OrignalLength,Length):-distance(Start,SecondStep,GeneratedDistance),
-                                    \+member(SecondStep,[GenHead|GenTail]),
-                                    TotalDistance is OrignalLength+GeneratedDistance,
+                                    TotalDistance is OrignalLength+GeneratedDistance, %Evalutes total path distance
                                     path_gen(SecondStep,End,[SecondStep,GenHead|GenTail],Answer,TotalDistance,Length).
 
-
+% Finds the appropraite length of each path.
 find_paths_length(Start,End,Answer,Length):- path_gen(Start,End,[Start],GenAnswer,0,Length),
                                reverse(GenAnswer,ReversedAnswer),
                                Answer = ReversedAnswer.
-combine_paths(Start,End,Generated):-findall((Length,Answer),find_paths_length(Start,End,Answer,Length),Generated).
-combined_path_sorter(Start,End):-combine_paths(Start,End,Generated),
-                            sort(Generated,SortedSolution),
-                            head(SortedSolution,FirstElement),
-                            head(FirstElement,Length,Route),
-                            writef(' The route: %d is shortest with with length: %d', [Route,Length]).
+
+% Generates an overall list with the associated lengths and routes.
+combine_paths(Start,End,Generated):-
+                  findall((Length,Answer),find_paths_length(Start,End,Answer,Length),Generated).
+
+% ######### Main Function ###############
+% Appropriatly formates the shortes path output to the user.
+shortest_path(Start,End):-
+        combine_paths(Start,End,Generated),
+        sort(Generated,SortedSolution),
+        head(SortedSolution,FirstElement),
+        head(FirstElement,Length,Route),
+        writef(' The route: %d is shortest with with length: %d', [Route,Length]).
 
 % % Currently Working solution, no bidirection.
 % oh(1, 2, 1).
